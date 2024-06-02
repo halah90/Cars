1267641abef3d1a57b935bd9b19c0368a5e727d7
halahsami901 <halahsami901@gmail.com>
2024-06-02 20:52:37 +0300

    Add car controller

---
+++ Cars/Controllers/CarController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cars.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+    }
+}
